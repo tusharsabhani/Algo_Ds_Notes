94e01452a555301333bf254c38af0794df8fc2ae
tusharsabhani <37148864+tusharsabhani@users.noreply.github.com>
2019-04-06 19:07:48 +0530

    added function

--- DDA_Line_Drawing_Algorithm/DDA_Line_Drawing_Algo.c
+++ DDA_Line_Drawing_Algorithm/DDA_Line_Drawing_Algo.c
@@ -1,19 +1,10 @@
 #include<stdio.h>
-#include<conio.h>
 #include<stdlib.h>
 #include<graphics.h>
-void main()
+
+void dda(int x1, int x2, int y1, int y2)
 {
-    float x1,x2,y1,y2,dx,dy,steps,xi,yi,i;
-    //xi=x increment, yi=y increment
-    int gdriver=DETECT,gmode,errorcode;
-    initgraph(&gdriver,&gmode,"");
-
-    printf("Enter the starting co-ordinates of line.");
-    scanf("%f%f%",&x1,&y1);
-    printf("Enter the ending co-ordinates of line.");
-    scanf("%f%f",&x2,&y2);
-
+    float dx, dy, steps, xi, yi, i;
     dx = x2 - x1;
     dy = y2 - y1;
 
@@ -25,13 +16,26 @@ void main()
     xi = dx / steps;
     yi = dy / steps;
 
-    for(i=1;i<steps;i++)
+    for(i = 1; i < steps; i++)
     {
-        putpixel(x1,y1,15);
+        putpixel(x1, y1, 15);
         x1 = x1 + xi ;
         y1 = y1 + yi;
     }
-    getch();
+}
+
+void main()
+{
+    float x1, x2, y1, y2;
+    //xi=x increment, yi=y increment
+    int gdriver = DETECT, gmode, errorcode;
+    initgraph(&gdriver, &gmode, "");
+
+    printf("Enter the starting co-ordinates of line.");
+    scanf("%f%f%", &x1, &y1);
+    printf("Enter the ending co-ordinates of line.");
+    scanf("%f%f", &x2, &y2);
+
     closegraph();
 
 }
--- DDA_Line_Drawing_Algorithm/DDA_Line_Drawing_Algo.cpp
+++ DDA_Line_Drawing_Algorithm/DDA_Line_Drawing_Algo.cpp
@@ -1,22 +1,10 @@
 #include<stdio.h>
 #include<iostream.h>
-#include<conio.h>
 #include<stdlib.h>
 #include<graphics.h>
-void main()
+
+void dda(int x1, int x2, int y1, int y2)
 {
-    int gdriver=DETECT,gmode,errorcode;
-    initgraph(&gdriver,&gmode,"");
-
-    //x1 and y1 are initial coordinates of line
-    //x2 and y2 are final coordinates of line
-    float x1,x2,y1,y2;
-
-    cout<<"Enter the starting coordinates of line = ";
-    cin>>x1>>y1;
-    cout<<"Enter the ending co-ordinates of line = ";
-    cin>>x2>>y2;
-
     float dx = x2 - x1;
     float dy = y2 - y1;
 
@@ -26,18 +14,35 @@ void main()
     else
 	steps = abs(dy);
 
-    float xi,yi;
+    float xi, yi;
     //xi=x increment, yi=y increment
     xi = dx / steps;
     yi = dy / steps;
 
     for(int i=1;i<steps;i++)
     {
-        putpixel(x1,y1,15);
+        putpixel(x1, y1, 15);
         x1 = x1 + xi ;
         y1 = y1 + yi;
     }
-    getch();
+}
+
+void main()
+{
+    int gdriver = DETECT, gmode, errorcode;
+    initgraph(&gdriver, &gmode, "");
+
+    //x1 and y1 are initial coordinates of line
+    //x2 and y2 are final coordinates of line
+    float x1, x2, y1, y2;
+
+    cout<<"Enter the starting coordinates of line = ";
+    cin>>x1>>y1;
+    cout<<"Enter the ending co-ordinates of line = ";
+    cin>>x2>>y2;
+
+    dda(int x1, int x2, int y1, int y2);
+    
     closegraph();
 
 }
